6f6d78278d63f047ed99abcc5234572ed5819a43
haftamudesta <desta451616@hotmail.com>
2022-07-23 23:05:50 +0300

    modify

--- form.js
+++ form.js
@@ -5,12 +5,29 @@ const textArea = document.querySelector('#Text-Area');
 const errorMessage = document.querySelector('.error-message');
 function validateform() {}
 function isemailValid() {}
-let email;
+ let email;
 form.addEventListener('submit', (events) => {
-  events.preventDefault();
-    if (emailinput.value.trim() === '') {
+    events.preventDefault();
+    if (userName.value.trim() === '') {
+        errorMessage.style.display = "block";
+        errorMessage.textContent = 'name can not be empty';
+        
+    }  
+    else if (emailinput.value.trim() === '') {
         errorMessage.textContent = 'email can not be empty';
     }
-    else
-        form.remove(); 
+    else if (!isemailValid(emailinput.value)) {
+        errorMessage.textContent = 'email is not valid';
+    }
+    else if (textArea.value.trim() === '') {
+        errorMessage.textContent = 'text area can not be empty';
+    }
+    else {
+        errorMessage.style.display = "none";
+    }
 });
+function isemailValid(email);
+  {
+    const re = /^[a-z-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    return re.test(email);
+  }
