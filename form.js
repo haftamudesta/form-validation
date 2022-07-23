2624a2ce139ea0e82ede71fadf60daba11d53458
haftamudesta <desta451616@hotmail.com>
2022-07-23 17:41:45 +0300

    Modify form.js

--- form.js
+++ form.js
@@ -7,29 +7,34 @@ function validateform() {
 }
 function isemailValid() {
 }
+function errorMessages() {
+}
 let email;
+let message
 form.addEventListener('submit', (events) => {
   events.preventDefault();
   validateform();
 });
 validateform(); {
   if (userName.value.trim() === '') {
-    errorMessage.style.display = 'block';
-    errorMessage.textContent = 'name can not be empty';
+    errorMessages('name can not be empty')
   } else if (emailinput.value.trim() === '') {
-    errorMessage.style.display = 'block';
-    errorMessage.textContent = 'email can not be empty';
+    errorMessages('email can not be empty')
   } else if (!isemailValid(emailinput.value.trim())) {
-    errorMessage.style.display = 'block';
-    errorMessage.textContent = 'invalid email,please provide valid email';
+    errorMessages('invalid email,please provide valid email')
   } else if (TextArea.value.trim() === '') {
-    errorMessage.style.display = 'block';
-    errorMessage.textContent = 'text area can not be empty';
+      errorMessage.style.display = 'block';
+      errorMessages('text area can not be empty')
+    
   } else {
     errorMessage.style.display = 'none';
   }
 }
 isemailValid(email); {
-  const re =/^[a-z-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  const re = /^[a-z-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
   return re.test(String(email));
+}
+errorMessages(message);{
+    errorMessage.style.display = 'block';
+    errorMessage.textContent =message ;
 }
