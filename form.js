e3cf0c6f9b9391cc529599c73a744faf34ffd496
haftamudesta <desta451616@hotmail.com>
2022-07-23 22:40:44 +0300

    modify

--- form.js
+++ form.js
@@ -12,16 +12,8 @@ form.addEventListener('submit', (events) => {
 });
 validateform();
 {
-  if (userName.value.trim() === '') {
-    errorMessage.textContent = 'name can not be empty';
-  } else if (emailinput.value.trim() === '') {
-    errorMessage.textContent = 'email can not be empty';
-  } else if (!isemailValid(emailinput.value)) {
-    errorMessage.textContent = 'invalid email, please enter a valid email';
-  } else if (textArea.value.trim() === '') {
-    errorMessage.textContent = 'text area can not be empty';
-  } else {
-    form.remove();
+    if (emailinput.ariaValueMax.trim() === '') {
+        errorMessage.textContent = 'email can not be empty';
   }
 }
 // isemailValid(email);
