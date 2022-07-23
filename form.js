78f3bcce564e53ce5979f91de0ed9c39e992a6b9
haftamudesta <desta451616@hotmail.com>
2022-07-23 20:15:27 +0300

    modify

--- form.js
+++ form.js
@@ -35,8 +35,7 @@ function validateform() {
         errorMessage.style.display = "none"; 
     }
 }
-function isemailValid();
-  {
+function isemailValid(){
     const re =/^[a-zA-Z0-9][\-_\.\+\!\#\$\%\&\'\*\/\=\?\^\`\{\|]{0,1}([a-zA-Z0-9][\-_\.\+\!\#\$\%\&\'\*\/\=\?\^\`\{\|]{0,1})*[a-zA-Z0-9]@[a-zA-Z0-9][-\.]{0,1}([a-zA-Z][-\.]{0,1})*[a-zA-Z0-9]\.[a-zA-Z0-9]{1,}([\.\-]{0,1}[a-zA-Z]){0,}[a-zA-Z0-9]{0,}$/i
     return re.test(String(email));
   }
